b126396f31d6587c1869a5b9d35fd60f25025b0f
佐藤秀嗣 <>
2019-01-20 22:51:38 +0900

    edited index.html&css Added index.js

---
+++ js/index.js
@@ -0,0 +1,246 @@
+$(function () {
+  $('#openModal').click(function(){
+      $('#modalArea').fadeIn();
+  });
+  $('#openModal2').click(function(){
+      $('#modalArea2').fadeIn();
+  });
+  $('#openModal3').click(function(){
+      $('#modalArea3').fadeIn();
+  });
+  $('#openModal4').click(function(){
+      $('#modalArea4').fadeIn();
+  });
+  $('#openModal5').click(function(){
+      $('#modalArea5').fadeIn();
+  });
+  $('#openModal6').click(function(){
+      $('#modalArea6').fadeIn();
+  });
+  $('#openModal7').click(function(){
+      $('#modalArea7').fadeIn();
+  });
+  $('#openModal8').click(function(){
+      $('#modalArea8').fadeIn();
+  });
+  $('#openModal9').click(function(){
+      $('#modalArea9').fadeIn();
+  });
+  $('#openModal10').click(function(){
+      $('#modalArea10').fadeIn();
+  });
+  $('#openModal11').click(function(){
+      $('#modalArea11').fadeIn();
+  });
+  $('#openModal12').click(function(){
+      $('#modalArea12').fadeIn();
+  });
+  $('#openModal13').click(function(){
+      $('#modalArea13').fadeIn();
+  });
+  $('#openModal14').click(function(){
+      $('#modalArea14').fadeIn();
+  });
+  $('#openModal15').click(function(){
+      $('#modalArea15').fadeIn();
+  });
+  $('#openModal16').click(function(){
+      $('#modalArea16').fadeIn();
+  });
+  $('#openModal17').click(function(){
+      $('#modalArea17').fadeIn();
+  });
+  $('#openModal18').click(function(){
+      $('#modalArea18').fadeIn();
+  });
+  $('#openModal19').click(function(){
+      $('#modalArea19').fadeIn();
+  });
+  $('#openModal20').click(function(){
+      $('#modalArea20').fadeIn();
+  });
+  $('#openModal21').click(function(){
+      $('#modalArea21').fadeIn();
+  });
+  $('#openModal22').click(function(){
+      $('#modalArea22').fadeIn();
+  });
+  $('#openModal23').click(function(){
+      $('#modalArea23').fadeIn();
+  });
+  $('#openModal24').click(function(){
+      $('#modalArea24').fadeIn();
+  });
+  $('#openModal25').click(function(){
+      $('#modalArea25').fadeIn();
+  });
+  $('#openModal26').click(function(){
+      $('#modalArea26').fadeIn();
+  });
+  $('#openModal27').click(function(){
+      $('#modalArea27').fadeIn();
+  });
+  $('#openModal28').click(function(){
+      $('#modalArea28').fadeIn();
+  });
+  $('#openModal29').click(function(){
+      $('#modalArea29').fadeIn();
+  });
+  $('#openModal30').click(function(){
+      $('#modalArea30').fadeIn();
+  });
+  $('#openModal31').click(function(){
+      $('#modalArea31').fadeIn();
+  });
+  $('#openModal32').click(function(){
+      $('#modalArea32').fadeIn();
+  });
+  $('#openModal33').click(function(){
+      $('#modalArea33').fadeIn();
+  });
+  $('#openModal34').click(function(){
+      $('#modalArea34').fadeIn();
+  });
+  $('#openModal35').click(function(){
+      $('#modalArea35').fadeIn();
+  });
+  $('#openModal36').click(function(){
+      $('#modalArea36').fadeIn();
+  });
+  $('#openModal37').click(function(){
+      $('#modalArea37').fadeIn();
+  });
+  $('#openModal38').click(function(){
+      $('#modalArea38').fadeIn();
+  });
+  $('#openModal39').click(function(){
+      $('#modalArea39').fadeIn();
+  });
+  $('#openModal40').click(function(){
+      $('#modalArea40').fadeIn();
+  });
+
+
+
+  $('#closeModal , #modalBg').click(function(){
+    $('#modalArea').fadeOut();
+  });
+  $('#closeModal2 , #modalBg2').click(function(){
+    $('#modalArea2').fadeOut();
+  });
+  $('#closeModal3 , #modalBg3').click(function(){
+    $('#modalArea3').fadeOut();
+  });
+  $('#closeModal4 , #modalBg4').click(function(){
+    $('#modalArea4').fadeOut();
+  });
+  $('#closeModal5 , #modalBg5').click(function(){
+    $('#modalArea5').fadeOut();
+  });
+  $('#closeModal6 , #modalBg6').click(function(){
+    $('#modalArea6').fadeOut();
+  });
+  $('#closeModal7 , #modalBg7').click(function(){
+    $('#modalArea7').fadeOut();
+  });
+  $('#closeModal8 , #modalBg8').click(function(){
+    $('#modalArea8').fadeOut();
+  });
+  $('#closeModal9 , #modalBg9').click(function(){
+    $('#modalArea9').fadeOut();
+  });
+  $('#closeModal10 , #modalBg10').click(function(){
+    $('#modalArea10').fadeOut();
+  });
+  $('#closeModal11 , #modalBg11').click(function(){
+    $('#modalArea11').fadeOut();
+  });
+  $('#closeModal12 , #modalBg12').click(function(){
+    $('#modalArea12').fadeOut();
+  });
+  $('#closeModal13 , #modalBg13').click(function(){
+    $('#modalArea13').fadeOut();
+  });
+  $('#closeModal14 , #modalBg14').click(function(){
+    $('#modalArea14').fadeOut();
+  });
+  $('#closeModal15 , #modalBg15').click(function(){
+    $('#modalArea15').fadeOut();
+  });
+  $('#closeModal16 , #modalBg16').click(function(){
+    $('#modalArea16').fadeOut();
+  });
+  $('#closeModal17 , #modalBg17').click(function(){
+    $('#modalArea17').fadeOut();
+  });
+  $('#closeModal18 , #modalBg18').click(function(){
+    $('#modalArea18').fadeOut();
+  });
+  $('#closeModal19 , #modalBg19').click(function(){
+    $('#modalArea19').fadeOut();
+  });
+  $('#closeModal20 , #modalBg20').click(function(){
+    $('#modalArea20').fadeOut();
+  });
+  $('#closeModal21 , #modalBg21').click(function(){
+    $('#modalArea21').fadeOut();
+  });
+  $('#closeModal22 , #modalBg22').click(function(){
+    $('#modalArea22').fadeOut();
+  });
+  $('#closeModal23 , #modalBg23').click(function(){
+    $('#modalArea23').fadeOut();
+  });
+  $('#closeModal24 , #modalBg24').click(function(){
+    $('#modalArea24').fadeOut();
+  });
+  $('#closeModal25 , #modalBg25').click(function(){
+    $('#modalArea25').fadeOut();
+  });
+  $('#closeModal26 , #modalBg26').click(function(){
+    $('#modalArea26').fadeOut();
+  });
+  $('#closeModal27 , #modalBg27').click(function(){
+    $('#modalArea27').fadeOut();
+  });
+  $('#closeModal28 , #modalBg28').click(function(){
+    $('#modalArea28').fadeOut();
+  });
+  $('#closeModal29 , #modalBg29').click(function(){
+    $('#modalArea29').fadeOut();
+  });
+  $('#closeModal30 , #modalBg30').click(function(){
+    $('#modalArea30').fadeOut();
+  });
+  $('#closeModal31 , #modalBg31').click(function(){
+    $('#modalArea31').fadeOut();
+  });
+  $('#closeModal32 , #modalBg32').click(function(){
+    $('#modalArea32').fadeOut();
+  });
+  $('#closeModal33 , #modalBg33').click(function(){
+    $('#modalArea33').fadeOut();
+  });
+  $('#closeModal34 , #modalBg34').click(function(){
+    $('#modalArea34').fadeOut();
+  });
+  $('#closeModal35 , #modalBg35').click(function(){
+    $('#modalArea35').fadeOut();
+  });
+  $('#closeModal36 , #modalBg36').click(function(){
+    $('#modalArea36').fadeOut();
+  });
+  $('#closeModal37 , #modalBg37').click(function(){
+    $('#modalArea37').fadeOut();
+  });
+  $('#closeModal38 , #modalBg38').click(function(){
+    $('#modalArea38').fadeOut();
+  });
+  $('#closeModal39 , #modalBg39').click(function(){
+    $('#modalArea39').fadeOut();
+  });
+  $('#closeModal40 , #modalBg40').click(function(){
+    $('#modalArea40').fadeOut();
+  });
+
+});
